3e632d906faf128746a4bfa133108db108bfa75c
nnafar <nnafar1996@gmail.com>
2023-04-23 13:27:54 +0200

    Changed comments and added descriptions

--- Nikki/curvature_module.py
+++ Nikki/curvature_module.py
@@ -2,7 +2,15 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+Module containing functions to compute the mean curvature of objects in a .tif image file.
+
+The code was inspired by:
+    https://ojskrede.github.io/inf4300/exercises/week_11/
+    https://github.com/jmschabdach/caulobacter-curvature/blob/master/Identifying%20and%20Measuring%20the%20Curvature%20of%20Caulobacter%20Cells.ipynb
+    https://stackoverflow.com/questions/9137216/python-edge-detection-and-curvature-calculation
+    https://scikit-image.org/skimage-tutorials/lectures/1_image_filters.html
+    https://towardsdatascience.com/image-segmentation-using-pythons-scikit-image-module-533a61ecc980
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,7 +21,7 @@ import skimage.segmentation as segmentation
 import skimage.measure as measure
 import scipy.ndimage as ndi
 
-#%% Function that calculates the curvatures defined by a set of points
+#%% Function to calculates the curvatures defined by a set of points
 
 def calculate_curvature(points):
     # Calculate first and second derivatives of the curve
@@ -25,15 +33,16 @@ def calculate_curvature(points):
     
     return curvature
 
-#%% Function that processes the image by smoothing, manipulating, and detecting edges
-def preprocess_image(image, sigma=3, dilation_radius=2, min_size=200):
+#%% Function to pre-processes image; e.g., smoothing, manipulating, and detecting edges
+
+def preprocess_image(image, sigma, dilation_radius, min_size):
     # Smooth edges with Gaussian filter
     smooth_image = filters.gaussian(image, sigma=sigma)
 
-    # Use Canny edge detection algorithm to detect edges 
+    # Canny edge detection algorithm to detect edges 
     edges = feature.canny(smooth_image)
 
-    # Dilate the edges using a disk-shaped element with the specified radius
+    # Dilate edges using disk-shaped element with specified radius
     dilated_edges = morphology.dilation(edges, selem=morphology.disk(dilation_radius))
 
     # Fill the insides of the objects
@@ -41,59 +50,48 @@ def preprocess_image(image, sigma=3, dilation_radius=2, min_size=200):
 
     # Remove objects smaller than the specified minimum size
     cleaned_edges = morphology.remove_small_objects(filled_edges, min_size=min_size)
+    
     return cleaned_edges
 
+#%% Function to segment pre-processed image using the Watershed algorithm 
+
 def segment_objects(cleaned_edges):
-    '''
-    Segments objects in a preprocessed binary image using the watershed algorithm.
-
-    Input:
-        cleaned_edges (NumPy array): A preprocessed binary image.
-
-    Returns:
-        NumPy array: A labeled image with segmented objects.
-    '''
-    # Compute the Euclidean distance transform of the cleaned_edges image
+    # Euclidean distance transform of image
     distance = ndi.distance_transform_edt(cleaned_edges)
 
-    # Find local maxima in the distance image, indicating the approximate centers of objects
-    local_maxi = feature.peak_local_max(distance, indices=False, footprint=np.ones((3, 3)), labels=cleaned_edges)
+    # Local maxima (of distance image), approximates the centers of objects
+    local_max = feature.peak_local_max(distance, indices=False, footprint=np.ones((3, 3)), labels=cleaned_edges)
 
-    # Label the local maxima to create markers for the watershed algorithm
-    markers = ndi.label(local_maxi)[0]
+    # Create markers for the watershed algorithm from the local maxima
+    markers = ndi.label(local_max)[0]
 
     # Apply the watershed algorithm using the markers and the inverted distance image
     labels = segmentation.watershed(-distance, markers, mask=cleaned_edges)
+    
     return labels
 
-def plot_segmented_objects(image, labels, contour_threshold=0.8):
-    '''
-    Plots the segmented objects along with their mean curvature on the original image.
+#%% Function to plot segmented objects along with their mean curvature on the original image. 
 
-    Input:
-        image (NumPy array): The original grayscale image.
-        labels (NumPy array): The labeled image with segmented objects.
-        contour_threshold (float): Threshold for finding contours in the labeled image.
-    '''
-    # Find the contours of the segmented objects in the labeled image
+def plot_segmented_objects(image, labels, contour_threshold):
+    # Find the contours of the segmented objects
     contours = measure.find_contours(labels, contour_threshold)
 
-    # Calculate the curvature of each contour and its mean curvature
+    # Calculate the each contour's curvature and their mean curvature
     curvatures = [calculate_curvature(contour) for contour in contours]
     mean_curvatures = [np.mean(curv) for curv in curvatures]
 
-    # Create a plot with the original image and draw the contours
+    # Plot with the original image and draw the contours
     fig, ax = plt.subplots(figsize=(10, 10))
     ax.imshow(image, cmap='gray')
 
-    # Iterate through the contours and mean curvatures, plotting and annotating each object
+    # Go through each object and plot with annotations
     for contour, mean_curv in zip(contours, mean_curvatures):
         ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-        centroid = np.mean(contour, axis=0)
-        ax.annotate(f"{mean_curv:.2f}", (centroid[1], centroid[0]), color='red', fontsize=12)
+        center = np.mean(contour, axis=0)
+        ax.annotate(f"{mean_curv:.2f}", (center[1], center[0]), color='red', fontsize=12)
 
-    # Add labels and titles to the plot
-    ax.set_xlabel('X axis (pixels)')
-    ax.set_ylabel('Y axis (pixels)')
+    # Labels and titles to the plot
     ax.set_title('Segmented objects with mean curvature')
+    ax.set_xlabel('Number of pixels')
+    ax.set_ylabel('Number of pixels')
     plt.show()
